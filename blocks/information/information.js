90de16f726a0a849b640607df316b78bb0d41108
vgnnshvcky <vgnsh.vcky@gmail.com>
2025-01-03 06:14:55 -0500

    Update information.js

--- blocks/information/information.js
+++ blocks/information/information.js
@@ -1,1 +1,15 @@
+ // Code generated via "Slingshot" 
+export default function decorate(block) {
+  const container = document.createElement('div');
+  container.className = 'information-container';
+
+  const title = block.querySelector('h2');
+  const content = block.querySelector('.row:last-child');
+
+  if (title) container.appendChild(title);
+  if (content) container.appendChild(content);
+
+  block.textContent = '';
+  block.appendChild(container);
+}
 
