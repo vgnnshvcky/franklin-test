60183c8795b6cca4bdbb09e77fe83b30272a0388
vgnnshvcky <vgnsh.vcky@gmail.com>
2025-01-03 05:46:45 -0500

    Update information.js

--- blocks/information/information.js
+++ blocks/information/information.js
@@ -1,13 +1,1 @@
-export default function decorate(block) {
-  const container = document.createElement('div');
-  container.className = 'information-container';
 
-  const title = block.querySelector('h2');
-  const content = block.querySelector('p');
-
-  if (title) container.appendChild(title);
-  if (content) container.appendChild(content);
-
-  block.textContent = '';
-  block.appendChild(container);
-}
