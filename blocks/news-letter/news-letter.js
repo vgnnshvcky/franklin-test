547c9139f7e23661a1fff06185d6ac2e4825a7c6
vgnnshvcky <vgnsh.vcky@gmail.com>
2025-01-03 12:08:31 -0500

    Update news-letter.js

--- blocks/news-letter/news-letter.js
+++ blocks/news-letter/news-letter.js
@@ -1,16 +1,20 @@
 export default function decorate(block) {
   const container = document.createElement('div');
-  container.className = 'newsletter-container';
+  container.className = 'news-letter-container';
 
   const title = block.querySelector('h2');
   const button = block.querySelector('a');
 
-  if (title) container.appendChild(title);
+  const innerContainer = document.createElement('div');
+  innerContainer.className = 'news-letter-inner';
+
+  if (title) innerContainer.appendChild(title);
   if (button) {
     button.className = 'button';
-    container.appendChild(button);
+    innerContainer.appendChild(button);
   }
 
+  container.appendChild(innerContainer);
   block.textContent = '';
   block.appendChild(container);
 }
