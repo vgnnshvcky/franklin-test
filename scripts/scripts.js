d5c70313fccdb72d3cecefcac97564a3d562d941
vgnnshvcky <vgnsh.vcky@gmail.com>
2025-01-03 13:14:24 -0500

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -16,7 +16,7 @@ import {
 /**
  * Builds hero block and prepends to main in a new section.
  * @param {Element} main The container element
- */
+ 
 function buildHeroBlock(main) {
   const h1 = main.querySelector('h1');
   const picture = main.querySelector('picture');
@@ -26,7 +26,7 @@ function buildHeroBlock(main) {
     section.append(buildBlock('hero', { elems: [picture, h1] }));
     main.prepend(section);
   }
-}
+}*/
 
 /**
  * load fonts.css and set a session storage flag
